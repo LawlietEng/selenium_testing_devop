48a88c18f479b8acf7917db95c712e219e7545fc
Ahmed Fadil <94470065+ahmedfadil@users.noreply.github.com>
2023-01-05 10:22:16 +0530

    Update FirstTestCase.py

--- FirstTestCase.py
+++ FirstTestCase.py
@@ -4,6 +4,7 @@ import string
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service
 serv_obj=Service("C:\driver\chromedriver_win32.zip\chromedriver")
 
@@ -14,7 +15,7 @@ driver.implicitly_wait(20)
 
 driver.get("http://127.0.0.1:8000/")
 time.sleep(1)
-driver.find_element(By.XPATH, "//*[@id='header']/div/ul/li/a").click()
+driver.find_element(By.XPATH, "//*[@id='header']/div/a/ul/li").click()
 time.sleep(1)
 driver.find_element(By.ID, "Newusn").send_keys("Wrong USN")
 time.sleep(1)
@@ -22,9 +23,11 @@ driver.find_element(By.ID, "Password").send_keys("randompw")
 time.sleep(1)
 driver.find_element(By.ID, "Login").click()
 time.sleep(1)
-driver.find_element(By.XPATH, "//*[@id='header']/div/ul/li/a").click()
+driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div/button").click()
 time.sleep(1)
-driver.find_element(By.XPATH, "//*[@id='header']/div/ul/li/a").click()
+#driver.find_element(By.XPATH, "//*[@id='header']/div/a/ul/li").click()
+#time.sleep(1)
+driver.find_element(By.XPATH, "//*[@id='header']/div/a/ul").click()
 time.sleep(1)
 
 ele1 = driver.find_element(By.ID, "CollegeName")
@@ -64,7 +67,7 @@ time.sleep(1)
 pw=random.randint(0,15000)
 driver.find_element(By.ID,"password").send_keys(pw)
 driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
-time.sleep(4)
+time.sleep(1)
 
 #var= driver.find_element(By.XPATH, "//*[@id='SignUpBtn']")
 #driver.execute_script("arguments[0].scrollIntoView();",var)
@@ -73,20 +76,45 @@ time.sleep(4)
 
 
 driver.find_element(By.ID,"ConfiPass").send_keys(pw)
-time.sleep(2)
+time.sleep(1)
 
 driver.find_element(By.ID,"SignUpBtn").click()
 time.sleep(2)
+driver.find_element(By.XPATH,"/html/body/div[1]/div/div[2]/div/button").click()
+time.sleep(1)
 driver.find_element(By.ID, "Newusn").send_keys(usn)
 time.sleep(1)
 driver.find_element(By.ID, "Password").send_keys(pw)
 time.sleep(1)
 driver.find_element(By.ID, "Login").click()
-time.sleep(100)
+time.sleep(1)
+driver.find_element(By.XPATH,"/html/body/div/div/div[2]/div/button").click()
+time.sleep(1)
+#hovering over js fundamentals
+admin=driver.find_element(By.XPATH,"//*[@id='CourseContainer']/div[1]/div/h2")
+usermgt=driver.find_element(By.ID,"JavaScript Fundamentals")
+actions=ActionChains(driver)
+actions.move_to_element(admin).move_to_element(usermgt).click().perform()
+time.sleep(1)
+driver.find_element(By.XPATH,"//*[@id='CardContainer']/div[4]/div/div[1]/img").click()
+time.sleep(20)
 
-
-
-
-
-
-#time.sleep(100)
+#this part of the code wont work if database is changed
+driver.get("http://127.0.0.1:8000/student_signin")
+driver.find_element(By.ID,"Newusn").send_keys("123")
+time.sleep(1)
+driver.find_element(By.ID,"Password").send_keys("123")
+time.sleep(1)
+driver.find_element(By.ID, "Login").click()
+time.sleep(1)
+driver.find_element(By.XPATH,"/html/body/div/div/div[2]/div/button").click()
+time.sleep(1)
+#hovering over data science
+admin1=driver.find_element(By.XPATH,"//*[@id='CourseContainer']/div[2]/div/h2")
+usermgt1=driver.find_element(By.ID,"Data Science")
+actions1=ActionChains(driver)
+actions1.move_to_element(admin1).move_to_element(usermgt1).click().perform()
+time.sleep(1)
+driver.find_element(By.XPATH,"//*[@id='CardContainer']/div[4]/div/div[1]/img").click()
+time.sleep(4)
+driver.close()
